56608b73d89b87eb13298ae57ac2a70dbe171e95
Jakob Long <jakob.long@utexas.edu>
2023-12-04 10:11:23 -0600

    Code works!

--- FEMSolver.py
+++ FEMSolver.py
@@ -1,149 +1,160 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# Jakob Long, JRL4725
 
-# Function to return the original function
+# returns the original function
 def f(x, t):
-    return ((np.pi ** 2 - 1) * np.exp(-t) * np.sin(np.pi * x))
+    return ((np.pi**2 - 1) * np.exp(-t) * np.sin(np.pi * x))
 
-# Function to return the analytical solution
+# returns the analytical solution
 def u_analytical(x, t):
     return (np.exp(-t) * np.sin(np.pi * x))
 
-# Function to return u(x,0) = sin(pi*x)
+# spatial conditions, at t = 1
 def u_boundary(x):
-    return (np.sin(np.pi * x))
+    return np.sin(np.pi * x)
 
-# Initialization of necessary matrices & step sizes
-def initializations(N, n):
-    ts = np.linspace(0, 1, n + 1)
-    xi = np.linspace(0, 1, N)
-    dt = 1 / n
-    h = xi[1] - xi[0]
-    local2global_map = np.vstack((np.linspace(0, N - 2, N - 1), np.linspace(1, N - 1, N - 1))).T
-    return ts, xi, dt, h, local2global_map
+# initalizes our stiffness, mass & force matrices
+def initialize_matrices(N,n):
+    M = np.zeros((N, N))
+    K = np.zeros((N, N))
+    Fg = np.zeros((N, n + 1))
+    return M, K, Fg
 
-# Creates phi test functions
-def phi_functions(h):
+# function to convert from local nodal mapping to global
+def construct_local_to_global_map(N):
+    return np.vstack((np.linspace(0, N - 2, N - 1), np.linspace(1, N - 1, N - 1))).T
+
+# Creates our basis functions, based on quadrature
+def generate_local_basis_functions(h):
     phi_1 = lambda eta: (1 - eta) / 2
     phi_2 = lambda eta: (1 + eta) / 2
 
     dphi = np.array([-1 / 2, 1 / 2])
-
-    dx_deta = h / 2
     deta_dx = 2 / h
+    dx_deta = h / 2
 
-    weight_1 = -1 / np.sqrt(3)
-    weight_2 = 1 / np.sqrt(3)
+    eta_quad_1 = -1 / np.sqrt(3)
+    eta_quad_2 = 1 / np.sqrt(3)
 
-    phi_eta = np.array([[phi_1(weight_1), phi_1(weight_2)],
-                        [phi_2(weight_1), phi_2(weight_2)]])
-
-    return phi_eta, deta_dx, dx_deta, dphi
-
-# Constructs matrices for stiffness, force, and applies boundary conditions
-def stiff_force_matrices(N,n,phi_eta, local2global_map, h, deta_dx, dphi, dx_deta, ts):
-    Kloc = np.zeros((2, 2))
-    Mloc = np.zeros((2, 2))
-    Kg = np.zeros((N, N))
-    Mg = np.zeros((N, N))
-    Fg = np.zeros((N, n + 1))
+    phi_eta = np.array([[phi_1(eta_quad_1), phi_1(eta_quad_2)],
+                        [phi_2(eta_quad_1), phi_2(eta_quad_2)]])
     
+    return phi_eta, dphi, deta_dx, dx_deta
+
+# Constructs matrices for stiffness, mass & force
+def calculate_matrices(N,ts,K,M,Fg, local2global_map, phi_eta, dphi, deta_dx, dx_deta,h):
     for k in range(N - 1):
+        mlocal = np.zeros((2, 2))
+        klocal = np.zeros((2, 2))
+
         for l in range(2):
             for m in range(2):
-                Mloc[l, m] = ((phi_eta[l, 0] * phi_eta[m, 0] + phi_eta[l, 1] * phi_eta[m, 1]) * h)
-                Kloc[l, m] = dphi[l] * deta_dx * dphi[m] * deta_dx * dx_deta * 2
+                mlocal[l, m] = (phi_eta[l, 0] * phi_eta[m, 0] + phi_eta[l, 1] * phi_eta[m, 1]) * h
+                klocal[l, m] = dphi[l] * deta_dx * dphi[m] * deta_dx * dx_deta * 2
 
         for l in range(2):
             global_node = int(local2global_map[k, l])
-            global_node = max(0, min(N - 1, global_node))
-
+            global_node = max(0, min(N - 1, global_node))  # Bound the indices
             for m in range(2):
                 global_node2 = int(local2global_map[k, m])
-                global_node2 = max(0, min(N - 1, global_node2))
-                Kg[global_node, global_node2] += Kloc[l, m]
-                Mg[global_node, global_node2] += Mloc[l, m]
+                global_node2 = max(0, min(N - 1, global_node2))  # Bound the indices
+                K[global_node, global_node2] += klocal[l, m]
+                M[global_node, global_node2] += mlocal[l, m]
+        Fg[k, :] = -(f((-1/np.sqrt(3)), ts) * phi_eta[0,0] + f((1/np.sqrt(3)), ts) * phi_eta[0,1]) * (1 / 8)
+    return M, K, Fg
 
-        Fg[k, :] = -(f(-1 / np.sqrt(3), ts) * phi_eta[0, 0] + f(1 / np.sqrt(3), ts) * phi_eta[0, 1]) * (1 / 8)
+# Applies & constructs boundary conditions of u(0,t) = u(1,t) = 0
+def apply_boundary_conditions(M, N):
+    M[0, :] = 0
+    M[:, 0] = 0
+    M[N - 1, :] = 0
+    M[:, N - 1] = 0
+    M[0, 0] = 1
+    M[N - 1, N - 1] = 1
+    
+    bounds = [0,0]
+    dirichlet_bc = np.eye(N)
+    dirichlet_bc[0,0] = bounds[0]
+    dirichlet_bc[N-1,N-1] = bounds[1]
+    return M, dirichlet_bc
 
-    Mg[0, :] = 0
-    Mg[:, 0] = 0
-    Mg[N - 1, :] = 0
-    Mg[:, N - 1] = 0
-    Mg[0, 0] = 1
-    Mg[N - 1, N - 1] = 1
+# Solves matrix operations necessary to perform Euler approximations
+def matrix_operations(M,K,dt):
+    invM = np.linalg.inv(M)
+    MK = np.dot(invM, K)
+    B = (1/dt) * M + K
+    invB = np.linalg.inv(B)
+    
+    return invM, MK, B, invB
 
-    return Kg, Mg, Fg
-
-# Applies & constructs boundary conditions
-def boundary_conditions(xi, N, n):
+# Performs Forward or Backward euler approximation
+def solve_equation(N, n, dt, MK, invM,M, Fg, dbc, method,xi,invB):
     u = np.zeros((N, n + 1))
     u[:, 0] = u_boundary(xi)
 
-    boundaries = [0, 0]
-    dirichlet_bc = np.eye(N)
-    dirichlet_bc[0, 0] = boundaries[0]
-    dirichlet_bc[N - 1, N - 1] = boundaries[1]
+    if method == 'FE':
+        for z in range(n):
+            u[:, z + 1] = u[:, z] - dt * MK.dot(u[:, z]) + dt * invM.dot(Fg[:, z])
+            u[:, z + 1] = dbc.dot(u[:, z + 1])
+    else:
+        for z in range(n):
+            u[:, z + 1] = (1 / dt) * invB.dot(M.dot(u[:, z])) + invB.dot(Fg[:, z])
+            u[:, z + 1] = dbc.dot(u[:, z + 1])
 
-    return u, dirichlet_bc
-
-# Forward Euler method
-def forward_euler(u, dt, MK, InvMg, Fg, dbc, n):
-    for t in range(n - 1):
-        u[:, t + 1] = u[:, t] - dt * MK.dot(u[:, t]) + dt * InvMg.dot(Fg[:, t])
-        u[:, t + 1] = dbc.dot(u[:, t + 1])
     return u
 
-# Backward Euler method
-def backward_euler(u, dt, invB, Mg, Fg, dbc, n):
-    for t in range(n):
-        u[:, t + 1] = (1 / dt) * invB.dot(Mg.dot(u[:, t])) + invB.dot(Fg[:, t])
-        u[:, t + 1] = dbc.dot(u[:, t + 1])
-    return u
+# Plots our approximation against analytical solution
+def plot_solutions(xn, sol, x, u, n,method):
+    plt.plot(xn, sol, label='Analytical Solution')
+    if method == 'FE':
+        plt.plot(x, u[:, n], label=f'Forward Euler Solution with n = {n}')
+    else:
+        plt.plot(x, u[:, n], label=f'Backward Euler Solution with n = {n}')
+    plt.xlabel('x')
+    plt.ylabel('Solution')
+    plt.title('Comparison of Analytical and Numerical Solutions')
+    plt.legend()
+    plt.show()
 
-# Main function
 def main():
     N = int(input("Enter the number of Nodes: "))
     n = int(input("Enter the number of time steps: "))
-
-    ts, xi, dt, h, local2global_map = initializations(N, n)
-    phi_eta, dx_deta, deta_dx, dphi = phi_functions(h)
-    Kg, Mg, Fg = stiff_force_matrices(N,n,phi_eta, local2global_map, h, deta_dx, dphi, dx_deta, ts)
-    InvMg = np.linalg.inv(Mg)
-    MK = np.dot(InvMg, Kg)
-    B = ((1 / dt) * Mg) + Kg
-    InvB = np.linalg.inv(B)
-    u, dbc = boundary_conditions(xi, N, n)
-
+    xi = np.linspace(0,1,N)
+    h = xi[1] - xi[0]
+    dt = 1/n
+    ts = np.linspace(0,1,n+1)
+    
     while True:
         method = str(input("Please type FE for Forward Euler or Please type BE for Backward Euler: ").upper())
         if method == 'FE':
-            u = forward_euler(u, dt, MK, InvMg, Fg, dbc, n)
-            print(u)
+            M, K, Fg = initialize_matrices(N,n)
+            local2global_map = construct_local_to_global_map(N)
+            phi_eta, dphi, deta_dx, dx_deta = generate_local_basis_functions(h)
+            M, K, Fg = calculate_matrices(N, ts, K, M, Fg, local2global_map, phi_eta, dphi, deta_dx, dx_deta,h)
+            M,dbc = apply_boundary_conditions(M, N)
+            invM, MK, B, invB = matrix_operations(M,K,dt)
+            u = solve_equation(N, n, dt, MK, invM, M, Fg, dbc, method,xi,invB)
             x = np.linspace(0, 1, N)
             xn = np.linspace(0, 1, 1000)
-            u_a = u_analytical(xn, 1)
-            plt.plot(xn, u_a, label='Exact Solution')
-            plt.plot(x, u[:, n - 1], label='Forward Euler Approximation')
-            plt.xlabel('x')
-            plt.ylabel('u')
-            plt.legend()
-            plt.show()
+            sol = u_analytical(xn,1)
+            plot_solutions(xn, sol, x, u, n, method)
             break
         elif method == 'BE':
-            u = backward_euler(u, dt, InvB, Mg, Fg, dbc, n)
+            M, K, Fg = initialize_matrices(N,n)
+            local2global_map = construct_local_to_global_map(N)
+            phi_eta, dphi, deta_dx, dx_deta = generate_local_basis_functions(h)
+            M, K, Fg = calculate_matrices(N, ts, K, M, Fg, local2global_map, phi_eta, dphi, deta_dx, dx_deta,h)
+            M,dbc = apply_boundary_conditions(M, N)
+            invM, MK, B, invB = matrix_operations(M,K,dt)
+            u = solve_equation(N, n, dt, MK, invM, M, Fg, dbc, method,xi,invB)
             x = np.linspace(0, 1, N)
             xn = np.linspace(0, 1, 1000)
-            u_a = u_analytical(xn, 1)
-            plt.plot(xn, u_a, label='Exact Solution')
-            plt.plot(x, u[:, n - 1], label='Forward Euler Approximation')
-            plt.xlabel('x')
-            plt.ylabel('u')
-            plt.legend()
-            plt.show()
+            sol = u_analytical(xn,1)
+            plot_solutions(xn, sol, x, u, n, method)
             break
         else:
             print("Please select FE or BE.")
-
+    
 if __name__ == "__main__":
     main()
